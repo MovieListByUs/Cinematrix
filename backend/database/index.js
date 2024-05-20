d2ec59b86f9a4dfca6decd60ab0b59954a8f8c0a
brushurteeth 8 <adem.hameed.123@gmail.com>
2024-05-20 13:22:05 +0100

    crud is on

---
+++ backend/database/index.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("movies", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+});
+const db = {};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.Movies = require("../models/movies.js")(sequelize, DataTypes);
+// db.Series = require("../../models/seriesModels.js")(sequelize, DataTypes);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("all good");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// sequelize
+//   .sync({ force: true })
+//   .then(() => {
+//     console.log("phrase table created successfully!");
+//   })
+//   .catch((error) => {
+//     console.error("Unable to create table : ", error);
+//   });
+
+module.exports = db;
